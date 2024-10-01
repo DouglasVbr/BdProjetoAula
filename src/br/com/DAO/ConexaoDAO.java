63a1f33739a40230ef88d9dd81cd5fcaf5ab817b
Douglas Vieira <130025057+DouglasVbr@users.noreply.github.com>
2024-10-01 17:21:24 -0300

    Add files via upload

---
+++ src/br/com/DAO/ConexaoDAO.java
@@ -0,0 +1,40 @@
+package br.com.DAO;
+
+import java.sql.*;
+//import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class ConexaoDAO {
+
+    public static Connection conector() {
+
+        java.sql.Connection conexao = null;
+
+        String driver = "com.mysql.jdbc.Driver";
+
+        String url = "jdbc:mysql://localhost:3306/bdprojetoaula";
+
+        String user = "root";
+
+        String password = "root";
+
+        try {
+            Class.forName(driver);
+
+            conexao = DriverManager.getConnection(url, user, password);
+
+            return conexao;
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "conexão falhou" + e.getMessage());
+
+            return null;
+
+        }
+
+    }
+}
